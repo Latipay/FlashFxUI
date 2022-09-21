ef737db0fb034518ec8f2e027c782567ead8f884
Thomas Wang <ts0516@gmail.com>
2022-09-21 17:28:40 +1200

    Update build script and related files

--- build.sh
+++ build.sh
@@ -1,5 +1,33 @@
 #!/bin/bash
 
-env=$1
+Env=$1
+env="`echo $Env | tr '[:upper:]' '[:lower:]'`"
+revision=$2
+dockerHubUsername=cfgglobal
+dockerHubPassword=$3
+imageTag=cfgglobal
+imageName="unique:flash-fx-payment-ui-$revision"
+publishFolder="dist-$env"
 
-yarn build --mode "$env" --dest dist-"$env"
+echo -e "\n\nEnv: $Env, env: $env, Version: $revision, ImageName: $imageName"
+
+if [[ $env == "" ]]; then
+    echo -e "Failed to convert $Env to lower case"
+    exit 1
+fi
+
+echo -e "\n node -v"
+node -v
+
+echo -e "\nLogging in docker hub..."
+docker login -u $dockerHubUsername -p "$dockerHubPassword"
+
+yarn build --mode "$env" --dest $publishFolder
+
+echo -e "\nBuilding docker image\n- docker build -f Dockerfile$Env . -t $imageTag/$imageName\n"
+docker build -f Dockerfile"$Env" . -t $imageTag/"$imageName"
+
+echo -e "\nPushing docker image to repo\n- docker push $imageTag/$imageName\n"
+docker push $imageTag/"$imageName"
+
+if [ -d $publishFolder ]; then rm -Rf $publishFolder; fi
